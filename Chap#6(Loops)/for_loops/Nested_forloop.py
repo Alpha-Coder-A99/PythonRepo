f99cd8e844214ff7c82aa3aa2e053e4a279d2cc8
AI Admin <coderalpha99@gmail.com>
2026-08-25 22:00:59 +0500

    Nested for loop

---
+++ Chap#6(Loops)/for_loops/Nested_forloop.py
@@ -0,0 +1,9 @@
+for week in range(1, 3): #
+    print(f"week {week}:")
+    for day in range(1, 4): 
+        print(f"  Din {day} coding done!")
+
+for bucket in range(1, 4): 
+    print(f"bucket Number {bucket} uthayi.")
+    for bartan in range(1, 6): 
+        print(f"  Bartan {bartan} dho liya! ✨")
