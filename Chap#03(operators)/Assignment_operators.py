d37cf8f4bd148c0df886e360e9025041fab94acd
AI Admin <coderalpha99@gmail.com>
2026-08-25 20:37:49 +0500

    Assignmentoperators

---
+++ Chap#03(operators)/Assignment_operators.py
@@ -0,0 +1,29 @@
+# 1. Simple Assignment (=)
+x = 10
+print(f"Shuruati value: {x}")
+
+# 2. Add and Assign (+=)
+x += 5  # Iska matlab hai: x = x + 5
+print(f"5 jama karne ke baad: {x}")
+
+# 3. Subtract and Assign (-=)
+x -= 3  # Iska matlab hai: x = x - 3
+print(f"3 nikalne ke baad: {x}")
+
+# 4. Multiply and Assign (*=)
+x *= 2  # Iska matlab hai: x = x * 2
+print(f"2 se multiply ke baad: {x}")
+
+# 5. Divide and Assign (/=)
+x /= 4  # Iska matlab hai: x = x / 4
+print(f"4 se divide ke baad: {x}")
+
+# 6. Modulus and Assign (%=)
+x = 10
+x %= 3  # 10 ko 3 se divide karo aur bacha hua (remainder) x mein daal do
+print(f"10 % 3 ka result: {x}")
+
+# 7. Exponent (Power) and Assign (**=)
+x = 2
+x **= 3 # 2 ki power 3 (2*2*2)
+print(f"2 ki power 3: {x}")
