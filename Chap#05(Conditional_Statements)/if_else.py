43cd06a2ed01281e1b4e90504178810bbc4d80f0
AI Admin <coderalpha99@gmail.com>
2026-08-25 21:09:47 +0500

    if_else

---
+++ Chap#05(Conditional_Statements)/if_else.py
@@ -0,0 +1,15 @@
+age=int(input("Enter your age:"))
+if age>=18:
+    print("You are adult👩 ,you can vote")
+else:
+    print("you can not vote, you are child👼👶")
+flame=int(input("Enter your flame"))
+if flame >=80:
+    print(f"your flame🔥 is {flame} which is high, salan jal sakta hn ")
+else:
+    print("salan yummy bna ga😋😋")
+Temperature=float(input("Enter tempreture"))
+if Temperature >= 30:
+    print("It is too hot 🥵")
+elif Temperature <= 15:
+    print("It is too cold 🥶") 
