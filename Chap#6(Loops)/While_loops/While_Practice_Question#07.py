1fd4743d836c4ffa90a518612623b024e11f7fb9
AI Admin <coderalpha99@gmail.com>
2026-08-25 21:30:21 +0500

    practice7

---
+++ Chap#6(Loops)/While_loops/While_Practice_Question#07.py
@@ -0,0 +1,10 @@
+"""Write a program that prints the sum of first n natural numbers
+ e.g,if n=5 then output dhould be :
+1+2+3+4+5=15(Hint:Keep a running tool iside loop)"""
+n=int(input("Enter natural number:"))
+sum_val=0 
+while n>=1:
+    sum_val+=n
+    n-=1
+print("sum:",sum_val)
+print("n=",n)
