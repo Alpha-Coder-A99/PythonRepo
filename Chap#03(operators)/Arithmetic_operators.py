677459923d75bbd546a500d3af8588e408917c3e
AI Admin <coderalpha99@gmail.com>
2026-08-25 20:35:07 +0500

    Arithmetic operators

---
+++ Chap#03(operators)/Arithmetic_operators.py
@@ -0,0 +1,18 @@
+# Arithmetic Operators 
+a = 15
+b = 4
+print("Basic Calculations:")
+print(f"Addition (15 + 4): {a + b}")         
+print(f"Subtraction (15 - 4): {a - b}")      
+print(f"Multiplication (15 * 4): {a * b}")  
+print(f"Division (15 / 4): {a / b}")         
+
+print("\nSpecial Calculations:")
+# Floor Division (Sirf poora numbr deta ha, point ke baad wala nahi)
+print(f"Floor Division (15 // 4): {a // b}") 
+
+# Modulus (Bacha hua hissa / Remainder)
+print(f"Remainder (15 % 4): {a % b}")        
+
+# Exponentiation (Power)
+print(f"Power (15^4): {a ** b}")             # 15*15*15*15
