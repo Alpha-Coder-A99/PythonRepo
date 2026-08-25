5573117350c82eb4f20a7da53adb3d4e90a51276
AI Admin <coderalpha99@gmail.com>
2026-08-25 20:43:43 +0500

    Membership_operators

---
+++ Chap#03(operators)/Membership_operators.py
@@ -0,0 +1,6 @@
+"""Membership Operators (in, not in)
+Ye check karte hain ke kya koi cheez kisi list 
+ya sequence ke andar hai ya nahi"""
+name = "Alpha Coder"
+print("Alpha" in name)   # True
+print("z" not in name)   # True
