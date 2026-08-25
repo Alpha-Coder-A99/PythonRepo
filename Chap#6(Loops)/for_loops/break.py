1af09fc0e86f3e64850ccff9926853739e209a12
AI Admin <coderalpha99@gmail.com>
2026-08-25 21:51:44 +0500

    break.py

---
+++ Chap#6(Loops)/for_loops/break.py
@@ -0,0 +1,18 @@
+# break	🛑 STOP	Loop ko foran khatam kar deta hai.
+for i in range(1, 11):
+    if i == 7:
+        print("Mil gaya 7! Ab agay nahi jana.")
+        break  # Loop yahin khatam! 🤞
+    print(i)
+
+
+
+correct_pass = "python123"
+
+for attempt in range(1, 6):
+    guess = input("Password dalo: ")
+    if guess == correct_pass:
+        print("Access Granted! 🔓")
+        break # Baaqi attempts ki zaroorat nahi, loop khatam!
+    else:
+        print(f"Ghalat! {5 - attempt} chances baaqi hain.")
