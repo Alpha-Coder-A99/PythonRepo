ba849297a3ceea92800b435ee0cb6e01539ae659
AI Admin <coderalpha99@gmail.com>
2026-08-25 21:53:14 +0500

    pass

---
+++ Chap#6(Loops)/for_loops/pass.py
@@ -0,0 +1,11 @@
+# pass	😶 NOTHING	Sirf jagah pur (fill) karta hai taake error na aaye.
+for i in range(1, 11):
+    if i == 5:
+        pass  # Kuch nahi karega, bas error se bachaye ga!
+    else:
+        print(i)
+
+def make_ai_robot():
+    pass # Abhi robot ka dimagh nahi banaya, baad mein banayenge!
+
+print("Program chal raha hai, koi error nahi!")
