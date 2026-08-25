80492b10cf9fc94b4e62337b4db0c0f5ded5bfcb
AI Admin <coderalpha99@gmail.com>
2026-08-25 20:41:27 +0500

    comparison operators

---
+++ Chap#03(operators)/comparison_operator.py
@@ -0,0 +1,25 @@
+# Comparison Operators 
+# Ye hamesha True ya False mein jawab dete hain
+
+x = 10
+y = 5
+
+print("Muqabla Shuru Karte Hain:")
+
+# 1. Equal to (==)
+print(f"Kya 10 barabar hai 5 ke? {x == y}")      # False
+
+# 2. Not Equal to (!=)
+print(f"Kya 10 barabar nahi hai 5 ke? {x != y}")  # True
+
+# 3. Greater than (>)
+print(f"Kya 10 bara hai 5 se? {x > y}")           # True
+
+# 4. Less than (<)
+print(f"Kya 10 chota hai 5 se? {x < y}")          # False
+
+# 5. Greater than or equal to (>=)
+print(f"Kya 10 bara ya barabar hai 10 ke? {x >= 10}") # True
+
+# 6. Less than or equal to (<=)
+print(f"Kya 5 chota ya barabar hai 2 ke? {y <= 2}")   # False
