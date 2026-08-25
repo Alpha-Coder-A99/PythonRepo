17230bef7e27e80ae494b3968bbcfd242900a731
AI Admin <coderalpha99@gmail.com>
2026-08-25 21:08:20 +0500

    if statemnt

---
+++ Chap#05(Conditional_Statements)/if_statement.py
@@ -0,0 +1,11 @@
+employed=input("choose True or False:")
+if employed:
+    print("Wonderful ....have fun!")
+
+adult=input("Are you adult,Yes or not:")
+if adult=="yes":
+    print("you are an adult")
+
+age=18
+if age >=18:
+    print("True")  
