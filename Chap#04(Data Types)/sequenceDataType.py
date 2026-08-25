064f2be2cc16a780bf3b6e3e6f77bc9922596e39
AI Admin <coderalpha99@gmail.com>
2026-08-25 21:00:14 +0500

    sequencetype

---
+++ Chap#04(Data Types)/sequenceDataType.py
@@ -0,0 +1,6 @@
+#Sequence Type(list,tuple,range,and also a str)
+list=["apple","Mango","ornge","banana"]#mutable
+print(list)
+tuple=("apple","banana","cheery")#immutable
+print(tuple)
+print(range(5))
