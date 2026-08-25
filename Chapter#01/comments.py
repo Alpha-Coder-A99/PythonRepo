de60ee0bbd6eebc47dc1be3349c444337302931b
Alpha-Coder-A99 <coderalpha99@gmail.com>
2026-08-25 10:04:43 +0500

    python basics

---
+++ Chapter#01/comments.py
@@ -0,0 +1,11 @@
+"Thier are two types of comments"
+"Coment are ued to exaplin code"
+"Comnt are notes you write for yourself , python completly ignore them they dont effect how program run"
+
+"1.single line commit"
+# This is single line comment
+
+"2.Multi line commit"
+""" This is
+multi-line
+comment """
--- Chapter#01/variables.py
+++ Chapter#01/variables.py
@@ -1,2 +1,4 @@
 a=3
 print(a)
+name="Alpha coder"
+print(name)
