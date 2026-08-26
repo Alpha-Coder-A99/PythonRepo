ed605bb364e4efd934cce40974c5010e36514839
AI Admin <coderalpha99@gmail.com>
2026-08-26 20:49:47 +0500

    pro4

---
+++ Chap#07(String)/Practice_Question#04.py
@@ -0,0 +1,9 @@
+# Write a program that inputs your fav food name and pints:
+# The middle 3 characters
+# The last 2 characters
+fav_foods=input("Enter your favourite food:")
+mid=(len(fav_foods))//2
+output1=fav_foods[mid-1:mid +2]
+print(output1)
+output2=fav_foods[-2:]
+print(output2)
