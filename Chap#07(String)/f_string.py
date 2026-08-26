54f64b74e3c82564aeaafd4699d7ed9cfec62d70
AI Admin <coderalpha99@gmail.com>
2026-08-26 20:30:10 +0500

    f_string

---
+++ Chap#07(String)/f_string.py
@@ -0,0 +1,18 @@
+name=input("Enter your name")
+print(f"my name is {name}")
+fruit=input("Enter yur fav fruit")
+print(f"l like {fruit}")
+
+name = "Areeba"
+age = 18
+goal = "AI Engineer"
+
+# Purana boring tareeqa:
+# print("Mera naam " + name + " hai aur main " + str(age) + " saal ki hoon.")
+
+# Naya Alpha Coder tareeqa (f-string):
+print(f"Mera naam {name} hai aur main {age} saal ki hoon.")
+print(f"Mera target 14 Sept tak {goal} banna hai!")
+
+naam = "areeba"
+print(f"Hello, {naam.upper()}!") # Output: Hello, AREEBA!
