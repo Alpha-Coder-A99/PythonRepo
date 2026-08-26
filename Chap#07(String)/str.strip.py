41b7f3257d060b190bde137b0032b701059feb78
AI Admin <coderalpha99@gmail.com>
2026-08-26 20:45:12 +0500

    .strip

---
+++ Chap#07(String)/str.strip.py
@@ -0,0 +1,23 @@
+naam = "   Alpha Coder   "
+
+print("Pehle wala:", naam, "|")
+# Output:    Alpha Coder |  
+
+clean_naam = naam.strip()
+print("Clean wala:", clean_naam, "|")
+# Output: Alpha Coder |
+ #.rsstrip
+msg = "Alpha Coder    "
+print(msg.rstrip()) 
+# Output: "Alpha Coder" (Right wali khali jagah khatam!)
+
+#lsstrip
+msg = "    Areeba"
+print(msg.lstrip()) 
+# Output: "Areeba" (Left wali khali jagah khatam!)
+
+website = "www.google.com"
+# Sirf shuru ka "www." hatana hai
+print(website.lstrip("w.")) # Output: "google.com"
+# Sirf aakhir ka ".com" hatana hai
+print(website.rstrip(".com")) # Output: "www.google"
