da468d1a97dac24ca149e86e460b67ce5dd42b29
AI Admin <coderalpha99@gmail.com>
2026-08-26 20:36:40 +0500

    Escape_Sequence

---
+++ Chap#07(String)/Escape_sequence.py
@@ -0,0 +1,17 @@
+# Agar aap chahti hain ke text agli line par chala jaye:
+print("Alpha\nCoder")
+# Output:
+# Alpha
+# Coder
+# 2. \t (Tab)
+# Ye text ke beech mein ek bari space (4 spaces jitni) de deta hai:
+print("Python\tAI")
+# Output: Python    AI
+# 3. \' ya \" (Quotes)
+# Agar aap ne string double quotes mein shuru ki hai aur andar bhi double quotes chahiye
+print("She said, \"Areeba is an Alpha Coder\"")
+# Output: She said, "Areeba is an Alpha Coder"
+# 4. \\ (Backslash)
+# Agar aap ko khud backslash print karna ho (kyunke ek backslash toh 'escape' ke liye use hota hai):
+print("C:\\Users\\Areeba")
+# Output: C:\Users\Areeba
