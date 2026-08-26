14c5c95430869e7922c93ef63e744ce8b1260172
AI Admin <coderalpha99@gmail.com>
2026-08-26 21:16:20 +0500

    .append

---
+++ Chap#08_List/list.append.py
@@ -0,0 +1,5 @@
+shopping_cart = ["flour", "sugar", "tea"]
+print("first list:", shopping_cart)
+shopping_cart.append("milk")
+shopping_cart.append("egg")
+print(" list after appending:", shopping_cart)  
