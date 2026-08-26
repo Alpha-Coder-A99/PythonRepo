125477fb216bcef2a71484f275dbc98e1d333655
AI Admin <coderalpha99@gmail.com>
2026-08-26 20:28:20 +0500

    str.replace

---
+++ Chap#07(String)/str.replace.py
@@ -0,0 +1,14 @@
+mood = "Main bohot sad hoon"
+new_mood = mood.replace("sad", "HAPPY")
+
+print(new_mood) 
+# Output: Main bohot HAPPY hoon
+
+text = "apple, apple, apple"
+print(text.replace("apple", "mango")) 
+# Output: mango, mango, mango
+
+dunya = "Log nafrat phela rahe hain"
+alpha_world = dunya.replace("nafrat", "MUHABBAT")
+print(alpha_world) 
+# Output: Log MUHABBAT phela rahe hain
