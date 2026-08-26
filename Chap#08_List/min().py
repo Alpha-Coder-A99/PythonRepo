7805509083ba4aa8a04a20a8c12269cff9d22002
AI Admin <coderalpha99@gmail.com>
2026-08-26 21:04:49 +0500

    min()

---
+++ Chap#08_List/min().py
@@ -0,0 +1,6 @@
+progress_scores = [10, 25, 18, 30, 22]
+
+# min() function check karega ke sab se miniumum value konsi ha
+Lowest_progress = min(progress_scores)
+
+print("your lowest progress is", Lowest_progress, "%")
