5160f9f4688ec942480aface767c66aef88b464f
AI Admin <coderalpha99@gmail.com>
2026-08-26 21:42:21 +0500

    Nestddict

---
+++ Chap#10_Dictionary/Nested_Dictionary.py
@@ -0,0 +1,16 @@
+student={
+    "name":"Fatima",
+    "Class":"2ndyear",
+    "Subjects":{
+        "Physics":96,
+        "Math":98,
+        "Computer":97,
+        "Urdu":95,
+        "English":94,
+        "Pakstudy":98
+
+    },
+    "Age":17
+
+}
+print(student)
