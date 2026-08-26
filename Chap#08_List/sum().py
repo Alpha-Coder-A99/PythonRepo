f93d13e32ead8e13b58bee158770ba26a4ce8632
AI Admin <coderalpha99@gmail.com>
2026-08-26 21:05:35 +0500

    sum()

---
+++ Chap#08_List/sum().py
@@ -0,0 +1,5 @@
+python_hours = [2, 3, 1, 4, 2]
+# sum() function check karega ke TOTAL kitne ghante kaam hua
+total_study_time = sum(python_hours)
+
+print("My python study hour:", total_study_time, "Hours")
