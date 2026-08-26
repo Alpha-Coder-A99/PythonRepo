dcc9f5f8d0967068e269f1de76e57a778fb9ac67
AI Admin <coderalpha99@gmail.com>
2026-08-26 21:03:41 +0500

    max()

---
+++ Chap#08_List/max().py
@@ -0,0 +1,6 @@
+progress_scores = [10, 25, 18, 30, 22]
+
+# max() function check karega ke sab se bari value konsi ha
+highest_progress = max(progress_scores)
+
+print("your highest progress is", highest_progress, "%")
