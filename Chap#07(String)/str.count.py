23723ccbb7358bb68d3297e1eefefc73ac12ebc4
AI Admin <coderalpha99@gmail.com>
2026-08-26 20:40:31 +0500

    .count

---
+++ Chap#07(String)/str.count.py
@@ -0,0 +1,13 @@
+shauq = "Python is great, Python is fast, Python is AI"
+
+# Ginti karo 'Python' kitni baar aaya?
+print(shauq.count("Python")) 
+# Output: 3
+
+# Ginti karo 'is' kitni baar aaya?
+print(shauq.count("is"))
+# Output: 3
+
+thoughts = "toxic, happy, toxic, happy, happy, focus"
+print("Toxic thoughts counted:", thoughts.count("toxic"))
+print("Happy thoughts counted:", thoughts.count("happy"))
