e4d1bfb468839ac38c14e398dba20d82c0ff5200
AI Admin <coderalpha99@gmail.com>
2026-08-26 21:32:47 +0500

    pro1

---
+++ Chap#08_Tuple/Practice_Question#03.py
@@ -0,0 +1,6 @@
+# Create a tuple of 5 favourite foods and then print:
+# 1.Total numbers of fruits
+#The index f one selected foods
+fav_foods=("Kheer","pulao","pizza","Choclate","kajo")
+print(len(fav_foods))
+print(fav_foods[4])
