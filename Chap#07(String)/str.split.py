6542085eeb08b2abd8fd48b907c3fee13cee3833
AI Admin <coderalpha99@gmail.com>
2026-08-26 20:44:04 +0500

    .split

---
+++ Chap#07(String)/str.split.py
@@ -0,0 +1,13 @@
+msg = "Alpha Coder Python AI"
+
+# String ko tukron mein banto
+tukray = msg.split()
+
+print(tukray)
+# Output: ['Alpha', 'Coder', 'Python', 'AI']
+
+fruits = "apple,mango,banana"
+list_of_fruits = fruits.split(",") # Comma se todo!
+
+print(list_of_fruits)
+# Output: ['apple', 'mango', 'banana']
