28f334c2aaa91cd11f1ffc21f8c9fc6b3bae163a
AI Admin <coderalpha99@gmail.com>
2026-08-26 21:20:51 +0500

    pro1

---
+++ Chap#08_List/Practice_Question#03.py
@@ -0,0 +1,11 @@
+# Quick Challenge for Alpha Coder:
+# Agar aap ke paas numbers ki list ho [1, 2, 3, 4, 5], aur aap pehle .pop() karein aur
+#  phir .reverse(), to final list kya hogi?
+
+# Step 1: nums = [1, 2, 3, 4, 5]
+# Step 2: nums.pop()
+# Step 3: nums.reverse()
+num=[1, 2, 3, 4, 5]
+num.pop()
+num.reverse()
+print(num)
