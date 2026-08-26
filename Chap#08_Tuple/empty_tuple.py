00c29ed4755ff58d69119e25d9cc228d3a8f77a2
AI Admin <coderalpha99@gmail.com>
2026-08-26 21:30:31 +0500

    emptytuple

---
+++ Chap#08_Tuple/empty_tuple.py
@@ -0,0 +1,6 @@
+#How to make empity tuple
+tup=()
+single_tuple=(1)#this one is not a tuple if we want to create tuple we have to put , must
+single_tuple=(1,)
+print(type(tup))
+print(type(single_tuple))
