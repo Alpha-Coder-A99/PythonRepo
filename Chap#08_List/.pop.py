5266cc42a436fcd5088f0a60280604e7f4b16c32
AI Admin <coderalpha99@gmail.com>
2026-08-26 21:18:04 +0500

    .pop

---
+++ Chap#08_List/.pop.py
@@ -0,0 +1,9 @@
+fruits = ["apple", "banana", "cherry"]
+removed_item = fruits.pop()# ya us ka defuald behavour hn ka hamesha last item pop ho ga
+
+print(removed_item) 
+print(fruits) 
+      
+removed_item=fruits.pop(1)
+print(removed_item)
+print(fruits)
