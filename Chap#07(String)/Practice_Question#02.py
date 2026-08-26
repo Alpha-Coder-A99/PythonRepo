b01ced5b66d56eb174bed7dc27cc5172b2afd44e
AI Admin <coderalpha99@gmail.com>
2026-08-26 20:49:01 +0500

    pro2

---
+++ Chap#07(String)/Practice_Question#02.py
@@ -0,0 +1,6 @@
+# Write a program that takes my fav fruit "Mango" vaiable and pints:
+# The middle 3 characters
+# The last 2 characters
+fav_foods="Mango"
+print(fav_foods[1:4])
+print(fav_foods[3:5]) 
