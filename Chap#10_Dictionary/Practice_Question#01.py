728b7fe56b535483e6c979ba8b529921c13483f3
AI Admin <coderalpha99@gmail.com>
2026-08-26 21:45:55 +0500

    pro1

---
+++ Chap#10_Dictionary/Practice_Question#01.py
@@ -0,0 +1,7 @@
+"""Create a dict name marks to store marks of 3 sunjects
+Add subject one by one and print final dictionary"""
+marks={}
+marks["Math"]=99
+marks["physics"]=98
+marks["Computer"]=96
+print(marks)
