0488299b2b6dd1aa1bd649b2eb9b2959a2768f2c
AI Admin <coderalpha99@gmail.com>
2026-08-26 21:00:39 +0500

    list

---
+++ Chap#08_List/len().py
@@ -0,0 +1,6 @@
+my_goals = ["Python", "Freelancing", "Tafseer", "Cooking", "AI"]
+
+# len() function  count kar ke batayega ke total kitne goals  hn life ma
+total_tasks = len(my_goals)
+
+print(f"I have {total_tasks} goals in my life")
