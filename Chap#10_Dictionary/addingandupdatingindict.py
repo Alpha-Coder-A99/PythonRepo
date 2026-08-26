88a51bf72559cce0aef19a854fb56d75e7432fcc
AI Admin <coderalpha99@gmail.com>
2026-08-26 21:43:10 +0500

    ading

---
+++ Chap#10_Dictionary/addingandupdatingindict.py
@@ -0,0 +1,14 @@
+# updating
+profile={
+    "key":"value ",
+    "name":"Alpha_Coder_99",
+    "Skills":"Python,Ai",
+    "Age":"17",
+    "Goal":"Ai devolpor"
+
+}
+profile["Skills"]= "python.Ai,Cooking"
+print(profile)
+#adding new key value pair
+profile["hobby"]="Cooking"
+print(profile)
