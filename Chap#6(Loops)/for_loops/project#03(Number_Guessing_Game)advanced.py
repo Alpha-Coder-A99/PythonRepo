9431b4535f746f3d6571615aabc47a4c9a507eda
AI Admin <coderalpha99@gmail.com>
2026-08-26 20:22:30 +0500

    project#03

---
+++ Chap#6(Loops)/for_loops/project#03(Number_Guessing_Game)advanced.py
@@ -0,0 +1,17 @@
+name=input("Welcome🥰 User! What is your name?")
+import random 
+computer=random.randint(1,100)
+tries=0
+
+while True:
+    tries+=1
+    human=int(input(f"hye!{name}!Guess your number between 1 - 100:-"))
+
+    if human==computer:
+        print(f"Congratualation!🎉{name}✨ you have win in {tries} tries!🎀")
+        break
+    elif human > computer:
+        print("WRONG Guess! Go lower !")
+    
+    elif  human < computer:
+        print("WRONG Guess! Go Higher !")
