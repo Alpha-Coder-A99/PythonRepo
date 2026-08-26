62fd0cd3b2b9afeddcfb21471ba0cab943634223
AI Admin <coderalpha99@gmail.com>
2026-08-26 21:02:07 +0500

    insert

---
+++ Chap#08_List/insert.py
@@ -0,0 +1,18 @@
+# .insert() ka matlab hai kisi cheez ko line tod kar apni marzi ki jagah par fit kar dena!
+list=["lichti","apple","mango","banana","Melon"]
+list.insert(1,"Dates")
+print(list)
+#program_2
+list=[1,2,3,4]
+list.insert(4,5)
+print(list)
+ 
+
+# my  daily routine
+routine = ["Python", "Cooking", "tafeer"]
+routine.insert(0, "sleep")
+
+# Ab hum chahte hain ke 'Exercise' Python ke baad aaye (Index 2)
+routine.insert(2, "Exercise")
+
+print("my Perfect Routine:", routine)
