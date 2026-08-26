bc08689496cde7497d44c287cc0a29f790bc6e48
AI Admin <coderalpha99@gmail.com>
2026-08-26 20:13:20 +0500

    pro9

---
+++ Chap#6(Loops)/for_loops/for_Practice_Question#09.py
@@ -0,0 +1,11 @@
+# print sum (even sum & odd sum seprately) by using for loop
+n=int(input("Enter your number:"))
+even_sum=0
+odd_sum=0
+for i in range(1,n+1):
+    if i%2==0:
+        even_sum+=i
+    else:
+        odd_sum+=i
+
+print(f"your even numbers sums are {even_sum} and your odd numbers are {odd_sum}")
