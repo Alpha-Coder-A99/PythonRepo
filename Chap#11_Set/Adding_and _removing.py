26b8c3778ca063fd149f18146d0b1db73b1e2838
AI Admin <coderalpha99@gmail.com>
2026-08-27 22:15:48 +0500

    adding removing

---
+++ Chap#11_Set/Adding_and _removing.py
@@ -0,0 +1,9 @@
+num={1,2,3,0}
+num.add(4)
+num.remove(0)
+print(num)
+
+foods={"Apple","Samosa","kheer","Cholapatora","Sandwatch","Golgappy"}
+foods.add("dates")
+foods.remove("Samosa") # because this is unhealthy😁😜
+print(foods)
