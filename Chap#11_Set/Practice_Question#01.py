8247c0af6678d8d0d3b7c665412c189f966841a6
AI Admin <coderalpha99@gmail.com>
2026-08-27 22:17:05 +0500

    pro1

---
+++ Chap#11_Set/Practice_Question#01.py
@@ -0,0 +1,9 @@
+# Take a list and convert it into set
+food_list=["Samosa","Sandwicth","GolGappy","Chanachat","Fruitchat","Samosa","Fruitchat"]
+print(len(food_list))
+print(type(food_list))
+#Converting into set
+food_Set=set(food_list)
+print(food_Set)
+print(type(food_Set))
+print(len(food_Set))
