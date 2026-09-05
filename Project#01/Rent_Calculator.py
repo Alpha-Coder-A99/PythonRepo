1e5e5fc4a418b9ea9a528cd567515251077d3df3
AI Admin <coderalpha99@gmail.com>
2026-09-05 21:51:49 +0500

    Rent_Calculator🧮🚀

--- Project#01/Rent_Calculator.py
+++ Project#01/Rent_Calculator.py
@@ -1,8 +1,8 @@
-rent=float(input("Enter your flat rent:"))
-food=float(input("Enter the amount of food odered:"))
-electricity_spend=float(input("Enter how much your electricity spend:"))
-Charge_per_unit=float(input("Enter Charge per unit:"))
-person=int(input("Enter the number of number who living in flat:"))
+rent=float(input("Enter your flat rent:-"))
+food=float(input("Enter the amount of food odered:-"))
+electricity_spend=float(input("Enter how much your electricity spend:-"))
+Charge_per_unit=float(input("Enter Charge per unit:-"))
+person=int(input("Enter the number of number who living in flat:-"))
 
 #Calcultion
 
