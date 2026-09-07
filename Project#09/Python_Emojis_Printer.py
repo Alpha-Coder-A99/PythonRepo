eaea08c52f149ad4a0ae82d3bf548612d56f7a97
AI Admin <coderalpha99@gmail.com>
2026-09-07 20:00:47 +0500

    Emoji_Printer😁😂🤗🥰☺🧐

---
+++ Project#09/Python_Emojis_Printer.py
@@ -0,0 +1,9 @@
+print("====WELCOME TO EMOJIS PRINTER====")
+print("Here are some cool emojis for you!")
+
+print("\N{Thinking Face} ")
+print("\N{Smiling Face with Sunglasses} ")
+print("\N{Face with Tears of Joy} ")
+print("\N{Party Popper} ")
+print("\N{Fire} ")
+print("\N{Face with Medical Mask} ")
