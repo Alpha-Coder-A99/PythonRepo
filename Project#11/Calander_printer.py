35b1ad870add8d68cc34e2dee41ca6f7ca4a3df2
AI Admin <coderalpha99@gmail.com>
2026-09-07 20:50:24 +0500

    Calander_printer.py

--- Project#11/Calander_printer.py
+++ Project#11/Calander_printer.py
@@ -1,5 +1,5 @@
-print("========WELCOME TO  CALANDER PRINTER========")
-from calendar import calendar
+print("========WELCOME TO  CALENDA_R PRINTER========")
+from calendar import calendar 
 
 year = int(input("Enter the year: "))
 print(calendar(year,m=2 ))
