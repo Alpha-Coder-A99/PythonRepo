3db5ff304359c292cd03848a4371b9ef2aca4e79
AI Admin <coderalpha99@gmail.com>
2026-09-07 21:00:00 +0500

    Calender_printer📆

--- Project#11/Calander_printer.py
+++
@@ -1,5 +0,0 @@
-print("========WELCOME TO  CALENDA_R PRINTER========")
-from calendar import calendar 
-
-year = int(input("Enter the year:- "))
-print(calendar(year,m=2 ))
---
+++ Project#11/Calender_printer.py
@@ -0,0 +1,5 @@
+print("========WELCOME TO  CALENDAR PRINTER========")
+from calendar import calendar 
+
+year = int(input("Enter the year:) "))
+print(calendar(year,m=2 ))
